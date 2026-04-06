5ab022ad1f53be54187aa82dae1654df88657cdb
迷渡 <justjavac@gmail.com>
2026-04-06 12:52:22 +0800

    fix: align native export declarations on windows

--- src/notification_native.h
+++ src/notification_native.h
@@ -17,7 +17,7 @@ MOONBIT_FFI_EXPORT int32_t desktop_notification_show(
     int32_t level);
 
 #ifdef _WIN32
-int32_t desktop_notification_windows_show(
+MOONBIT_FFI_EXPORT int32_t desktop_notification_windows_show(
     int64_t window_handle, moonbit_bytes_t title, moonbit_bytes_t body,
     int32_t level);
 #endif
@@ -27,8 +27,8 @@ int desktop_notification_run_process(char *const argv[]);
 #endif
 
 #ifdef __APPLE__
-int32_t desktop_notification_macos_is_supported(void);
-int32_t desktop_notification_macos_show(
+MOONBIT_FFI_EXPORT int32_t desktop_notification_macos_is_supported(void);
+MOONBIT_FFI_EXPORT int32_t desktop_notification_macos_show(
     int64_t window_handle, moonbit_bytes_t title, moonbit_bytes_t body,
     int32_t level);
 #endif
@@ -36,8 +36,8 @@ int32_t desktop_notification_macos_show(
 #ifdef __linux__
 int desktop_notification_find_program(
     const char *program, char *buffer, size_t buffer_size);
-int32_t desktop_notification_linux_is_supported(void);
-int32_t desktop_notification_linux_show(
+MOONBIT_FFI_EXPORT int32_t desktop_notification_linux_is_supported(void);
+MOONBIT_FFI_EXPORT int32_t desktop_notification_linux_show(
     int64_t window_handle, moonbit_bytes_t title, moonbit_bytes_t body,
     int32_t level);
 #endif
